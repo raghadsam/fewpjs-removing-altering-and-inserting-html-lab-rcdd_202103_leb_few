5b2fe707577854c11b35a3c8d6f33f748a3487d6
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-13 09:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Write your code here!
 main.remove();
+<<<<<<< HEAD
 let newHeader=document.createElement('h1');
 newHeader.id="victory";
 newHeader.textContent="Raghad is the champion"
+=======
+let newHeader=document.querySelector('h1');
+newHeader.appendChild('#victory');
+>>>>>>> ede8ee99d51a49fa1901b2c5daa604830a7e25e3
